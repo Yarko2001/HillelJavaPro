33615e555863e3a5e84fa58f6a8debebd89c3fc8
Yaroslav <merrynan2001@gmail.com>
2022-09-03 22:28:51 +0300

    added a console output of every figure

--- src/dehtiar/homeworks/homework_5/part_1/Main.java
+++ src/dehtiar/homeworks/homework_5/part_1/Main.java
@@ -7,6 +7,9 @@ import dehtiar.homeworks.homework_5.part_1.model.Triangle;
 public class Main {
     public static void main(String[] args) {
         Figure[] array = {new Circle(5), new Triangle(4, 4, 4), new Square(3)};
+        System.out.println("Square of circle :" + array[0].calculateSquare());
+        System.out.println("Square of triangle :" + array[1].calculateSquare());
+        System.out.println("Square of Square :" + array[2].calculateSquare());
         calculateAmountOfFigures(array);
     }
 
