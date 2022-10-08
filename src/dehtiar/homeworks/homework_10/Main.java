1c971ed34fd863a8052d404054f402ed91ce1490
Yaroslav <merrynan2001@gmail.com>
2022-10-08 21:27:42 +0300

    finished  homework_10

---
+++ src/dehtiar/homeworks/homework_10/Main.java
@@ -0,0 +1,12 @@
+package dehtiar.homeworks.homework_10;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    ValueCalculator valueCalculator = new ValueCalculator(1000000);
+    valueCalculator.doCalculate();
+
+  }
+
+}
